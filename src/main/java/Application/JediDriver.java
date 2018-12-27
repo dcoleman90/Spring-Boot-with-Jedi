7221f8e40f3cbab6c7bf5e7f8fc6a53022626ae0
dcoleman90 <dcolem12@my.westga.edu>
2018-12-27 13:58:22 -0500

    initial commit

---
+++ src/main/java/Application/JediDriver.java
@@ -0,0 +1,15 @@
+package Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JediDriver {
+
+    //Having troubles with the committing to github this comment is to allow another new commit
+    //see above
+    //Again above
+    public static void main(String args[]) {
+        SpringApplication.run(JediDriver.class, args);
+    }
+}
