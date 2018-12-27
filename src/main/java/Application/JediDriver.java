1cf96c27cdebd59894b1d9d80bb0ccb6313b39ed
dcoleman90 <dcolem12@my.westga.edu>
2018-12-27 14:05:32 -0500

    Removed unneeded comments

--- src/main/java/Application/JediDriver.java
+++ src/main/java/Application/JediDriver.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JediDriver {
-    
+    //testing pushing to git hub
     public static void main(String args[]) {
         SpringApplication.run(JediDriver.class, args);
     }
