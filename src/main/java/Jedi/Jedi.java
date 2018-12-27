beab3df7965d57834f72722fd696ba736036ae7f
dcoleman90 <dcolem12@my.westga.edu>
2018-12-27 16:19:31 -0500

    A basic POJO Jedi class is created and put initial code in the Jedi controller for it to return a hello world phrase

--- src/main/java/Jedi/Jedi.java
+++ src/main/java/Jedi/Jedi.java
@@ -1,5 +1,57 @@
 package Jedi;
 
 public class Jedi {
+    private String name;
+    private String race;
+    private int age;
+    private String lightSaberColor;
+    private String rank;
 
+    public Jedi(String name, String race, int age, String lightSaberColor, String rank) {
+        this.name = name;
+        this.race = race;
+        this.age = age;
+        this.lightSaberColor = lightSaberColor;
+        this.rank = rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLightSaberColor() {
+        return lightSaberColor;
+    }
+
+    public void setLightSaberColor(String lightSaberColor) {
+        this.lightSaberColor = lightSaberColor;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
 }
--- src/main/java/Jedi/JediController.java
+++ src/main/java/Jedi/JediController.java
@@ -1,5 +1,14 @@
 package Jedi;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class JediController {
 
+	
+	@RequestMapping("/Jedi")
+	public String sayHi() {
+		return "General Kenobi you are a bold one";
+	}
 }
